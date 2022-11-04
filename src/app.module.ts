f393ea2c80e7c36a7aa5d4148703edf5beedc6c6
mutoromike <mikemutoro@gmail.com>
2022-11-04 20:24:26 +0300

    First NestJS endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { ProductsModule } from './products/products.module';
+import { ProductsService } from './products/products.service';
+import { ProductsController } from './products/products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ 
+        ProductsModule, ],
+  controllers: [
+        ProductsController, AppController],
+  providers: [
+        ProductsService, AppService],
 })
 export class AppModule {}
